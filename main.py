db7e159b669010f5921eef883d0e8e81b3ca2315
Furkan <furkan@cybercore.com.au>
2025-03-08 00:31:28 +1100

    Add variables and data types

---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+# 1. Variables and Data Types
+name = "Furkan"
+age = 30
+weigh = 92.3
+heigh = 179
+is_student = True
+
+print(f"Name: {name}, Age: {age}, Weigh: {weigh}, Heigh: {heigh}, Student: {is_student}")
