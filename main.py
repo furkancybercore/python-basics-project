7da14665fd309a27ca15bbb8a11d17affb5a09a4
Furkan <furkan@cybercore.com.au>
2025-03-08 00:46:07 +1100

    Add dictionaries

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ fruits = ["apple", "banana", "cherry"]
 print("Fruits:")
 for fruit in fruits:
     print(f"- {fruit}")
-    
+
+# Dictionaries
+person = {
+    "name":"Betul",
+    "age": 29,
+    "is_student": False
+}    
+
+print(f"\nPerson Info:")
+for key, value, in person.items():
+    print(f"{key}:{value}")
